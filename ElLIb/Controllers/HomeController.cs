d549d2c10fe2f35b31ce932e231052df2c1440e3
LocalHost <cr.xeller@mail.ru>
2022-07-11 20:43:19 +0300

    Добавьте файлы проекта.

---
+++ ElLIb/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ElLIb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
