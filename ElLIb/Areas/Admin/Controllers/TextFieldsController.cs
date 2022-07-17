cb110a5fda92ce8169b2564d458a46f36b4504fd
LocalHost <cr.xeller@mail.ru>
2022-07-17 23:20:51 +0300

    bug fixed, add ServiceItems, how I can add new Books in my library and add to them images

---
+++ ElLIb/Areas/Admin/Controllers/TextFieldsController.cs
@@ -0,0 +1,33 @@
+﻿using ElLIb.Domain;
+using ElLIb.Domain.Entities;
+using ElLIb.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElLIb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class TextFieldsController : Controller
+    {
+        private readonly DataManager dataManager;
+        public TextFieldsController(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+
+        public IActionResult Edit(string codeWord)
+        {
+            var entity = dataManager.TextFields.GetTextFieldByCodeWord(codeWord);
+            return View(entity);
+        }
+        [HttpPost]
+        public IActionResult Edit(TextField model)
+        {
+            if (ModelState.IsValid)
+            {
+                dataManager.TextFields.SaveTextField(model);
+                return RedirectToAction(nameof(HomeController.Index), nameof(HomeController).CutController());
+            }
+            return View(model);
+        }
+    }
+}
